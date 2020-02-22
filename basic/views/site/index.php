f897c2c53afe3334e58a85b990f59d110d376809
Dmytro <dimasoltusyuk@gmail.com>
2020-02-22 13:10:28 +0200

    index fix

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,13 +6,13 @@
 
 <div class="site-index">
     <div class="row">
-        <div class="col-xs-6 col-md-4 col-xl-3">
+        <div class="col-xs-6 col-md-4 col-lg-3">
             <ul class="list-group col-xs-5 col-sm-4 col-md-3 col-lg-2" style="position: fixed;">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <a href="" class="list-group-item">Cras justo odioasdghasdjgasjgdgjasgdjagsdjas</a>
+                <a href="" class="list-group-item">Dapibus ac facilisis in</a>
+                <a href="" class="list-group-item">Morbi leo risus</a>
+                <a href="" class="list-group-item">Porta ac consectetur ac</a>
+                <a href="" class="list-group-item">Vestibulum at eros</a>
             </ul>
         </div>
         <section class="col-xs-6 col-md-8">
