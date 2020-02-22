6a2dbef3282c0e97ca372d028c060ca3b5cae1b5
Jedich <alexyudakov483@gmail.com>
2020-02-22 12:02:08 +0200

    migr create table group members

---
+++ basic/migrations/m200222_095714_create_table_group_members.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200222_095714_create_table_group_members
+ */
+class m200222_095714_create_table_group_members extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp() {
+    	$this->createTable("group_members", [
+    		'group_id' => $this->integer(11)->unsigned()->notNull(),
+			'user_id' => $this->integer(11)->unsigned()->notNull()
+		]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200222_095714_create_table_group_members cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200222_095714_create_table_group_members cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
