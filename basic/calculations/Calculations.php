cad1cc17c30ca542083c39e5481a554e2d59f7ad
PolinaPishova <ppoli162003@gmail.com>
2020-02-22 14:22:27 +0200

    EmptyFunc

---
+++ basic/calculations/Calculations.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace app\calculations;
+
+use app\models\GroupsInfo;
+class Calculations {
+
+    public function Calculations($payments, $members) {
+        foreach ($payments as $key => $value){
+            return 0;
+        }
+    }
+}
